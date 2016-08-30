dac06ad24550b7bdbe3ab9dd416473f78f3b4b21
tpechacek <taylortpechacek@gmail.com>
2016-08-30 14:54:49 -0400

    Hooked up image list to Imgur API

--- images/client/components/image_list.js
+++ images/client/components/image_list.js
@@ -3,17 +3,13 @@
 import React from 'react';
 import ImageDetail from './image_detail';
 
-const IMAGES = [
-  { title: 'Pen', link: 'http://dummyimage.com/600x400' },
-  { title: 'Pine Tree', link: 'http://dummyimage.com/600x400' },
-  { title: 'Mug', link: 'http://dummyimage.com/600x400' }
-];
-
 // Create our component
-const ImageList = () => {
-  const RenderedImages = IMAGES.map(function(image) {
-    return <ImageDetail image={image} />
-  });
+const ImageList = (props) => {
+  const validImages = props.images.filter(image => !image.is_album);
+
+  const RenderedImages = validImages.map(image =>
+    <ImageDetail key={image.title} image={image} />
+  );
 
   return (
     <ul className="media-list list-group">
