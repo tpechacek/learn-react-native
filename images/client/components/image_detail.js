c3b80234d9b447ff56e4b963012bf6d2f69c257a
tpechacek <taylortpechacek@gmail.com>
2016-09-02 17:34:51 -0700

    Finished Images example app

--- images/client/components/image_detail.js
+++ images/client/components/image_detail.js
@@ -1,5 +1,6 @@
 // Import the React Dictionairy
 import React from 'react';
+import ImageScore from './image_score';
 
 const ImageDetail = (props) => {
   // props.image => This is the image object
@@ -13,6 +14,8 @@ const ImageDetail = (props) => {
         <h4 className="media-heading">
           {props.image.title}
         </h4>
+        <p>{props.image.description}</p>
+        <ImageScore ups={props.image.ups} downs={props.image.downs} />
       </div>
     </li>
   );
