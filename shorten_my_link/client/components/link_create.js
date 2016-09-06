d9924320271fb0332f1facac446aaf31296605bf
tpechacek <taylortpechacek@gmail.com>
2016-09-06 12:12:18 -0700

    finished link shortener example app

--- shorten_my_link/client/components/link_create.js
+++ shorten_my_link/client/components/link_create.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 
 class LinkCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { error: '' };
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    Meteor.call('links.insert', this.refs.link.value, (error) => {
+      if (error) {
+        this.setState({ error: 'Enter a valid URL' });
+      } else {
+        this.setState({ error: '' });
+        this.refs.link.value = '';
+      }
+    });
+  }
+
   render () {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <div className="form-group">
-          <label>Linkt to shorten</label>
-          <input className="form-control" />
+          <label>Link to shorten</label>
+          <input ref="link" className="form-control" />
         </div>
+        <div className="text-danger">{this.state.error}</div>
         <button className="btn btn-primary">Shorten!</button>
       </form>
     );
