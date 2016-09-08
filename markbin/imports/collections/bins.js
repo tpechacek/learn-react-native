23dc979f567b12c53139a25bf3fd13f2af61c43e
tpechacek <taylortpechacek@gmail.com>
2016-09-07 23:56:12 -0400

    Finished markbin project

--- markbin/imports/collections/bins.js
+++ markbin/imports/collections/bins.js
@@ -12,6 +12,14 @@ Meteor.methods({
 
   'bins.remove': function(bin) {
     return Bins.remove(bin);
+  },
+
+  'bins.update': function(bin, content) {
+    return Bins.update(bin._id, { $set: { content: content } });
+  },
+
+  'bins.share': function(bin, email) {
+    return Bins.update(bin._id, { $push: { sharedWith: email } });
   }
 });
 
